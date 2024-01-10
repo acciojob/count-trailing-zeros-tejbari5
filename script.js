d9634383c6ad2eef12af5ab509ff536d17553e65
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2024-01-10 11:16:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let fact = factorial(n).toString();
+	let count = 0;
+	for(let i=fact.length - 1 ; i>=0 ; i--){
+		if(fact[i] != 0) return count;
+		count++;
+	}
+	return count;
+}
+
+function factorial(n){
+	if(n==0) return 1;
+	return n * factorial(n-1);
 }
 
 const input = prompt("Enter a number");
